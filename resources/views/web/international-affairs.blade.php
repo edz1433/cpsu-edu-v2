73d36ced0fdaf552968ff097921f6aa81f1b260e
edz1433 <edzavril1@gmail.com>
2025-09-18 14:55:30 +0800

    update

--- resources/views/web/international-affairs.blade.php
+++ resources/views/web/international-affairs.blade.php
@@ -1,14 +1,36 @@
 @extends('web.layouts.mainlayout')
 @section('content')
-<div class="container">
-    <div class="section-area section-sp1 ovbl-dark bg-fix online-cours" style="background-image:url(assets/images/count/cpsubg.png);">
-        <div class="container">
-            <div class="row">
-                <div class="col-md-12 text-center text-white">
-                    <iframe src="https://www.google.com/maps/embed?pb=!1m14!1m8!1m3!1d4043.5980337884957!2d122.88798265606688!3d9.851078830258214!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x33ac6d5da0e13cf7%3A0xa8d43186b79bc612!2sCentral%20Philippines%20State%20University!5e1!3m2!1sen!2sus!4v1695975626334!5m2!1sen!2sus" width="100%" height="450" style="border:2;" allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe>
-                </div>
-            </div>
-        </div>
-    </div>
-</div>
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Leaflet in Laravel</title>
+    <link rel="stylesheet" href="https://unpkg.com/leaflet/dist/leaflet.css"/>
+    <style>
+        #map {
+            height: 500px;
+        }
+    </style>
+</head>
+<body>
+    <h2>My Leaflet Map</h2>
+    <div id="map"></div>
+
+    <script src="https://unpkg.com/leaflet/dist/leaflet.js"></script>
+    <script>
+        // Initialize map
+        var map = L.map('map').setView([10.3157, 123.8854], 13); // Example: Cebu City
+
+        // Add OpenStreetMap tiles
+        L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+            attribution: '© OpenStreetMap contributors'
+        }).addTo(map);
+
+        // Marker
+        L.marker([10.3157, 123.8854]).addTo(map)
+            .bindPopup('Hello from Cebu!')
+            .openPopup();
+    </script>
+</body>
+</html>
+
 @endsection
